83ff5b412f57cae855633a5bc56fef3a277aee62
Hunar Roop Kahlon <hunar.roop@gmail.com>
2018-12-10 13:26:10 -0800

    add more fields
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/cli_opts.rs
+++ src/cli_opts.rs
@@ -1,9 +1,24 @@
+use std::path;
+
 #[derive(Debug, structopt::StructOpt)]
 #[structopt(name = "uuid")]
 pub struct CliOpts {
+    #[structopt(short = "-n")]
+    count: usize,
     /// Decode a `uuid`.
     #[structopt(name = "decode", short = "-d")]
     decode: bool,
-    #[structopt(short = "-v")]
-    uuid_version: String,
+    #[structopt(short = "-o", parse(from_os_str))]
+    filename: path::PathBuf,
+    #[structopt(short = "-F")]
+    format: String,
+    #[structopt(short = "-m")]
+    multicast: bool,
+    #[structopt(name = "namespace name")]
+    namespace: String,
+    #[structopt(short = "-1")]
+    reset_context: bool,
+    /// Specify a uuid version.
+    #[structopt(short = "-v", name = "uuid version")]
+    uuid_version: Option<String>,
 }
