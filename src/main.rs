6209915d62b38b6d3c8dd98c57e6741d9e241cea
Hunar Roop Kahlon <hunar.roop@gmail.com>
2018-12-17 09:59:17 -0800

    uuid v4 is generated as a fallback
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,7 @@ fn main() {
     use structopt::StructOpt;
 
     let opt = cli_opts::CliOpts::from_args();
-    println!("{:?}", opt);
+    println!("{:#?}", opt);
+
+    println!("{:x}", uuid::Uuid::new_v4())
 }
