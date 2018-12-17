0490c8bd5fe4a862b3a865147b1e3482584fd491
Hunar Roop Kahlon <hunar.roop@gmail.com>
2018-12-17 09:42:04 -0800

    change syntax to uuidgen style
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/cli_opts.rs
+++ src/cli_opts.rs
@@ -1,24 +1,42 @@
-use std::path;
-
 #[derive(Debug, structopt::StructOpt)]
 #[structopt(name = "uuid")]
 pub struct CliOpts {
-    #[structopt(short = "-n")]
-    count: usize,
-    /// Decode a `uuid`.
-    #[structopt(name = "decode", short = "-d")]
-    decode: bool,
-    #[structopt(short = "-o", parse(from_os_str))]
-    filename: path::PathBuf,
-    #[structopt(short = "-F")]
-    format: String,
-    #[structopt(short = "-m")]
-    multicast: bool,
-    #[structopt(name = "namespace name")]
-    namespace: String,
-    #[structopt(short = "-1")]
-    reset_context: bool,
-    /// Specify a uuid version.
-    #[structopt(short = "-v", name = "uuid version")]
-    uuid_version: Option<String>,
+    #[structopt(
+        long = "--hex",
+        short = "-x"
+    )]
+    hex: bool,
+    #[structopt(
+        long = "--md5",
+        short = "-m"
+    )]
+    md5: bool,
+    #[structopt(
+        long = "--name",
+        short = "-N"
+    )]
+    name: Option<String>,
+    #[structopt(
+        long = "--namespace",
+        short = "-n"
+    )]
+    namespace: Option<uuid::Uuid>,
+    #[structopt(
+        long = "--random",
+        short = "-r"
+    )]
+    random: bool,
+    #[structopt(
+        long = "--sha1",
+        short = "-s"
+    )]
+    sha1: bool,
+    #[structopt(
+        long = "--time",
+        raw(
+            conflicts_with_all = "&[\"random\"]"
+        ),
+        short = "-t"
+    )]
+    time: bool,
 }
