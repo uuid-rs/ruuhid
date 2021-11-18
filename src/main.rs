28582e91354990e2c780ce316055de096457790e
--get <hunar.roop@gmail.com>
2021-11-18 13:58:14 -0800

    initial working uuid gen
    Signed-off-by: --get <hunar.roop@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    let opts: RuuhidOpts = structopt::StructOpt::from_args();
+
+    if let Some(subcommand) = &opts.subcommand {
+    } else {
+        println!("{}", uuid::Uuid::new_v4())
+    }
+}
+
+#[derive(Debug, structopt::StructOpt)]
+#[structopt(name = "ruuhid")]
+struct RuuhidOpts {
+    #[structopt(subcommand)]
+    subcommand: Option<SubcommandOpts>,
+}
+
+#[derive(Debug, structopt::StructOpt)]
+enum SubcommandOpts {
+    #[structopt[aliases=&["gen"]]]
+    Generate(RuuhidGenOpts),
+}
+
+#[derive(Debug, structopt::StructOpt)]
+struct RuuhidGenOpts {
+    #[structopt(subcommand)]
+    subcommand: Option<RuuhidGenVersionOpts>,
+}
+
+#[derive(Debug, structopt::StructOpt)]
+enum RuuhidGenVersionOpts {
+    Nil,
+    Mac,
+    Dce,
+    Md5,
+    Random,
+    Sha1,
 }
