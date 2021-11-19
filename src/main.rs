e04cd525d103de14e5e6da74666885ef94c5384c
--get <hunar.roop@gmail.com>
2021-11-19 15:07:37 -0800

    remork the structopt structure
    Signed-off-by: --get <hunar.roop@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,85 @@
+use std::str;
+
 fn main() {
     let opts: RuuhidOpts = structopt::StructOpt::from_args();
 
-    if let Some(subcommand) = &opts.subcommand {
-    } else {
-        println!("{}", uuid::Uuid::new_v4())
+    match opts.subcommand {
+        None => {
+            println!("{}", uuid::Uuid::new_v4())
+        }
+        Some(opts) => match opts {
+            RuuhidSubOpts::Generate { subcommand } => match subcommand {
+                None => {
+                    println!("{}", uuid::Uuid::new_v4())
+                }
+                Some(opts) => match opts {
+                    RuuhidGenVersionOpts::Nil(_) => {
+                        println!("{}", uuid::Uuid::nil())
+                    }
+                    RuuhidGenVersionOpts::Mac => {}
+                    RuuhidGenVersionOpts::Dce => {}
+                    RuuhidGenVersionOpts::Md5 => {}
+                    RuuhidGenVersionOpts::Random => {
+                        println!("{}", uuid::Uuid::new_v4())
+                    }
+                    RuuhidGenVersionOpts::Sha1 => {}
+                },
+            },
+        },
     }
 }
 
-#[derive(Debug, structopt::StructOpt)]
-#[structopt(name = "ruuhid")]
+#[derive(Clone, Debug, structopt::StructOpt)]
 struct RuuhidOpts {
     #[structopt(subcommand)]
-    subcommand: Option<SubcommandOpts>,
+    subcommand: Option<RuuhidSubOpts>,
 }
 
-#[derive(Debug, structopt::StructOpt)]
-enum SubcommandOpts {
+#[derive(Clone, Debug, structopt::StructOpt)]
+enum RuuhidSubOpts {
     #[structopt[aliases=&["gen"]]]
-    Generate(RuuhidGenOpts),
+    // Generate(RuuhidGenOpts),
+    Generate {
+        #[structopt(subcommand)]
+        subcommand: Option<RuuhidGenVersionOpts>,
+    },
 }
 
-#[derive(Debug, structopt::StructOpt)]
-struct RuuhidGenOpts {
-    #[structopt(subcommand)]
-    subcommand: Option<RuuhidGenVersionOpts>,
-}
-
-#[derive(Debug, structopt::StructOpt)]
+#[derive(Clone, Debug, structopt::StructOpt)]
 enum RuuhidGenVersionOpts {
-    Nil,
+    #[structopt(aliases=&["0"])]
+    Nil(NilOpts),
+    #[structopt(aliases=&["1"])]
     Mac,
+    #[structopt(aliases=&["2"])]
     Dce,
+    #[structopt(aliases=&["3"])]
     Md5,
+    #[structopt(aliases=&["4"])]
     Random,
+    #[structopt(aliases=&["5"])]
     Sha1,
+}
+
+#[derive(Clone, Debug, structopt::StructOpt)]
+struct NilOpts {
+    #[structopt(long, short)]
+    format: FormatOpts,
+}
+
+#[derive(Clone, Debug, structopt::StructOpt)]
+enum FormatOpts {
+    Bytes,
+    Guid,
+    Hyphenated,
+    Simple,
+    Urn,
+}
+
+impl str::FromStr for FormatOpts {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        todo!()
+    }
 }
