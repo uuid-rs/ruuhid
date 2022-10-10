40f93ec3338a79004cf9ae0386904906ac09f68e
Hunar Roop Kahlon <hunar.roop@gmail.com>
2022-10-10 15:35:38 -0700

    switch directly to clap
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,36 @@
 use std::str;
 
 fn main() {
-    let opts: RuuhidOpts = structopt::StructOpt::from_args();
-
-    match opts.subcommand {
-        None => {
-            println!("{}", uuid::Uuid::new_v4())
-        }
-        Some(opts) => match opts {
-            RuuhidSubOpts::Generate { subcommand } => match subcommand {
-                None => {
-                    println!("{}", uuid::Uuid::new_v4())
-                }
-                Some(opts) => match opts {
-                    RuuhidGenVersionOpts::Nil(_) => {
-                        println!("{}", uuid::Uuid::nil())
-                    }
-                    RuuhidGenVersionOpts::Mac => {}
-                    RuuhidGenVersionOpts::Dce => {}
-                    RuuhidGenVersionOpts::Md5 => {}
-                    RuuhidGenVersionOpts::Random => {
-                        println!("{}", uuid::Uuid::new_v4())
-                    }
-                    RuuhidGenVersionOpts::Sha1 => {}
-                },
-            },
-        },
-    }
+    let opts: RuuhidOpts = clap::Parser::parse();
 }
 
-#[derive(Clone, Debug, structopt::StructOpt)]
-struct RuuhidOpts {
-    #[structopt(subcommand)]
-    subcommand: Option<RuuhidSubOpts>,
+#[derive(clap::Parser)]
+enum RuuhidOpts {
+   #[clap(aliases= &["g", "gen"])]
+    Generate(RuuhidGenerateOpts),
+    #[clap(aliases = &["p"])]
+    Parse,
 }
 
-#[derive(Clone, Debug, structopt::StructOpt)]
-enum RuuhidSubOpts {
-    #[structopt[aliases=&["gen"]]]
-    // Generate(RuuhidGenOpts),
-    Generate {
-        #[structopt(subcommand)]
-        subcommand: Option<RuuhidGenVersionOpts>,
-    },
+#[derive(clap::Parser)]
+#[clap(about)]
+struct RuuhidGenerateOpts {
+    #[clap(subcommand)]
+    version: RuuhidGenerateVersionOpts,
 }
 
-#[derive(Clone, Debug, structopt::StructOpt)]
-enum RuuhidGenVersionOpts {
-    #[structopt(aliases=&["0"])]
-    Nil(NilOpts),
-    #[structopt(aliases=&["1"])]
+#[derive(clap::Parser)]
+enum RuuhidGenerateVersionOpts {
+    #[clap(aliases = &["0", "v0"])]
+    Nil,
+    #[clap(aliases = &["1", "v1"])]
     Mac,
-    #[structopt(aliases=&["2"])]
+    #[clap(aliases = &["2", "v2"])]
     Dce,
-    #[structopt(aliases=&["3"])]
+    #[clap(aliases = &["3", "v3"])]
     Md5,
-    #[structopt(aliases=&["4"])]
+    #[clap(aliases = &["4", "v4"])]
     Random,
-    #[structopt(aliases=&["5"])]
+    #[clap(aliases = &["5", "v5"])]
     Sha1,
-}
-
-#[derive(Clone, Debug, structopt::StructOpt)]
-struct NilOpts {
-    #[structopt(long, short)]
-    format: FormatOpts,
-}
-
-#[derive(Clone, Debug, structopt::StructOpt)]
-enum FormatOpts {
-    Bytes,
-    Guid,
-    Hyphenated,
-    Simple,
-    Urn,
-}
-
-impl str::FromStr for FormatOpts {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!()
-    }
 }
