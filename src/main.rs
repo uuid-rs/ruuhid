9f03e6d508cbbd52a7d05cc908b0c4815163ea1d
Hunar Roop Kahlon <hunar.roop@gmail.com>
2022-10-12 00:21:12 -0700

    add ability to generate nil uuid
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,60 @@
 use std::str;
+use uuid;
 
 fn main() {
-    let opts: RuuhidOpts = clap::Parser::parse();
+    let opts: Opts = clap::Parser::parse();
+
+    match opts {
+        Opts::Generate(opts) => {
+            match opts.version {
+                GenerateVersionOpts::Nil(opts) => {
+                    (0..opts.number).for_each(|_| {
+                        let uuid = uuid::Uuid::nil();
+
+
+                        match (&opts.case, &opts.format) {
+                            (CaseOpts::Lower, FormatOpts::Hyphenated) => println!("{:x}", uuid.hyphenated()),
+                            (CaseOpts::Upper, FormatOpts::Hyphenated) => println!("{:X}", uuid.hyphenated()),
+                            (CaseOpts::Lower, FormatOpts::Simple) => println!("{:x}", uuid.simple()),
+                            (CaseOpts::Upper, FormatOpts::Simple) => println!("{:X}", uuid.simple()),
+                            (CaseOpts::Lower, FormatOpts::Urn) => println!("{:x}", uuid.urn()),
+                            (CaseOpts::Upper, FormatOpts::Urn) => println!("{:X}", uuid.urn()),
+                        }
+                    });
+                }
+                GenerateVersionOpts::Mac => {}
+                GenerateVersionOpts::Dce => {}
+                GenerateVersionOpts::Md5 => {}
+                GenerateVersionOpts::Random => {}
+                GenerateVersionOpts::Sha1 => {}
+            }
+        },
+        Opts::Parse => {
+
+        },
+    }
 }
 
 #[derive(clap::Parser)]
-enum RuuhidOpts {
+#[clap(about, author, version)]
+enum Opts {
    #[clap(aliases= &["g", "gen"])]
-    Generate(RuuhidGenerateOpts),
+    Generate(GenerateOpts),
     #[clap(aliases = &["p"])]
     Parse,
 }
 
 #[derive(clap::Parser)]
 #[clap(about)]
-struct RuuhidGenerateOpts {
+struct GenerateOpts {
     #[clap(subcommand)]
-    version: RuuhidGenerateVersionOpts,
+    version: GenerateVersionOpts,
 }
 
 #[derive(clap::Parser)]
-enum RuuhidGenerateVersionOpts {
+enum GenerateVersionOpts {
     #[clap(aliases = &["0", "v0"])]
-    Nil,
+    Nil(GenerateNilOpts),
     #[clap(aliases = &["1", "v1"])]
     Mac,
     #[clap(aliases = &["2", "v2"])]
@@ -33,4 +65,52 @@ enum RuuhidGenerateVersionOpts {
     Random,
     #[clap(aliases = &["5", "v5"])]
     Sha1,
+}
+
+#[derive(clap::Parser)]
+struct GenerateNilOpts {
+    #[clap(short, long, default_value = "lowercase")]
+    case: CaseOpts,
+    #[clap(short, long, default_value = "1")]
+    number: usize,
+    #[clap(short, long, default_value = "hyphenated")]
+    format: FormatOpts,
+}
+
+#[derive(Clone, clap::Parser)]
+enum FormatOpts {
+    Hyphenated,
+    Simple,
+    Urn,
+}
+
+impl str::FromStr for FormatOpts {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "hyphenated" | "hypen" | "h" => Ok(FormatOpts::Hyphenated),
+            "simple" | "s" => Ok(FormatOpts::Simple),
+            "urn" | "u" => Ok(FormatOpts::Urn),
+            _ => Err(format!("Invalid format: {}", s)),
+        }
+    }
+}
+
+#[derive(Clone, clap::Parser)]
+enum CaseOpts {
+    Lower,
+    Upper,
+}
+
+impl str::FromStr for CaseOpts {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "lowercase" | "lower" | "l" => Ok(CaseOpts::Lower),
+            "uppercase" | "upper" | "u" => Ok(CaseOpts::Upper),
+            _ => Err(format!("Invalid case: {}", s)),
+        }
+    }
 }
